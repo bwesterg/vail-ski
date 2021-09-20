0fdb77be4cc036beb1bd9efcc0cce0f961f25939
bwesterg <benton.westergaard@gmail.com>
2021-09-20 12:48:29 -0600

    De-uglify, partially, the new trail form on the page.

--- src/components/TrailContainer.js
+++ src/components/TrailContainer.js
@@ -9,15 +9,6 @@ export default function TrailContainer(props) {
 
     return (
         <div>
-            {/* <form>
-                <label for="difficulty">Choose a difficulty:</label>
-                    <select name="difficulty" id="status">
-                        <option value="green">GREEN</option>
-                        <option value="blue">BLUE</option>
-                        <option value="1black">1BLACK</option>
-                        <option value="2black">2BLACK</option>
-                    </select>
-            </form> */}
             <ul className="trail-container">
                 {showTrails()}
             </ul>
